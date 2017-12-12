47757fd4593ed6dda76e626a6e85e4d41f067d2f
Lucien Boillod <lucienboillod@gmail.com>
2017-12-12 01:10:13 +0100

    Test with hardcoded values

--- index.js
+++ index.js
@@ -16,4 +16,31 @@ const ad = mdns.createAdvertisement(mdns.tcp("http"), port, {
   },
 })
 
+const laserServoPath = path.join(__dirname, "..", "laser-servo")
+const moveHorizontal = path.join(laserServoPath, "move-horizontal")
+const moveVertical = path.join(laserServoPath, "move-vertical")
+
+async function moveRight() {
+  await execa(moveHorizontal + " 60")
+}
+
+async function moveLeft() {
+  await execa(moveHorizontal + " -60")
+}
+
+async function moveUp() {
+  await execa(moveVertical + " 60")
+}
+
+async function moveDown() {
+  await execa(moveVertical + " -60")
+}
+
+io.on("connection", socket => {
+  socket.on("moveRight", () => moveRight())
+  socket.on("moveLeft", () => moveLeft())
+  socket.on("moveUp", () => moveUp())
+  socket.on("moveDown", () => moveDown())
+})
+
 io.listen(port)
