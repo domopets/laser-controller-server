71df646641d5fd4508efc5a513af9c5b4826a9ed
Lucien Boillod <lucienboillod@gmail.com>
2018-01-25 00:33:39 +0100

    add Laser on/off

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ const ad = mdns.createAdvertisement(mdns.tcp("http"), port, {
 })
 
 const laserServoPath = path.join(__dirname, "..", "laser-servo")
+const on = path.join(laserServoPath, "on")
+const off = path.join(laserServoPath, "off")
 const moveHorizontal = path.join(laserServoPath, "move-horizontal")
 const moveVertical = path.join(laserServoPath, "move-vertical")
 
+<<<<<<< Updated upstream
 async function moveRight(angle) {
     await execa(moveHorizontal, [angle.toString()])
 }
@@ -39,6 +42,39 @@ io.on("connection", socket => {
   socket.on("moveLeft", (angle) => moveLeft(angle))
   socket.on("moveUp", (angle) => moveUp(angle))
   socket.on("moveDown", (angle) => moveDown(angle))
+=======
+async function moveRight() {
+  await execa(moveHorizontal, 60)
+}
+
+async function moveLeft() {
+  await execa(moveHorizontal, -60)
+}
+
+async function moveUp() {
+  await execa(moveVertical, 60)
+}
+
+async function moveDown() {
+  await execa(moveVertical, -60)
+}
+
+async function laserOn() {
+  await execa(on)
+}
+
+async function laserOff() {
+  await execa(off)
+}
+
+io.on("connection", socket => {
+  socket.on("moveRight", () => moveRight())
+  socket.on("moveLeft", () => moveLeft())
+  socket.on("moveUp", () => moveUp())
+  socket.on("moveDown", () => moveDown())
+  socket.on("laserOn", () => laserOn())
+  socket.on("laserOff", () => laserOff())
+>>>>>>> Stashed changes
 })
 
 io.listen(port)
